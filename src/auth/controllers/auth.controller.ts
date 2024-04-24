7dd2deab5d5e3b18dbeca560fb0426c3cc6499be
smi-23 <latack789@naver.com>
2024-04-24 17:24:44 +0900

    Feat: singup in user.service

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,17 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from '../services';
-import { SignupReqDto, SignupResDto } from '../dto';
+import { SignupReqDto } from '../dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService, private readonly singupResDto: SignupResDto) {}
+  constructor(
+    private readonly userService: UserService,
+    // private readonly singupResDto: SignupResDto,
+  ) {}
 
   @Get()
   testHello(): string {
     return this.userService.testHello();
   }
 
-  @Post('sighup')
+  @Post('signup')
   async signup(@Body() signupReqDto: SignupReqDto): Promise<SignupReqDto> {
     const user = await this.userService.signup(signupReqDto);
     return user;
--- src/auth/services/user.service.ts
+++ src/auth/services/user.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { UserRepository } from '../repositories';
+import { SignupReqDto } from '../dto';
+import { User } from '../entities';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
+  constructor(private readonly userRepository: UserRepository) {}
+
   testHello(): string {
     return 'test Hello';
+  }
+
+  async signup(singupReqDto: SignupReqDto): Promise<User> {
+    const hashedPassword = await argon2.hash(singupReqDto.password);
+    return this.userRepository.signup(singupReqDto, hashedPassword);
   }
 }
