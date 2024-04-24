f157bf049e4bd37c84d64d3b860487a237f63066
smi-23 <latack789@naver.com>
2024-04-24 16:54:56 +0900

    Feat: singup in authcontroller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from '../services';
+import { SignupReqDto, SignupResDto } from '../dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService, private readonly singupResDto: SignupResDto) {}
 
   @Get()
   testHello(): string {
     return this.userService.testHello();
+  }
+
+  @Post('sighup')
+  async signup(@Body() signupReqDto: SignupReqDto): Promise<SignupReqDto> {
+    const user = await this.userService.signup(signupReqDto);
+    return user;
   }
 }
