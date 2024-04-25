a438ee631fffe18903b6fd5de915d36f4756bb18
smi-23 <latack789@naver.com>
2024-04-25 13:08:30 +0900

    fix: typeorm연결 설정이 제대로 되지 않아 오류 발생 해결

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService, UserService } from './services';
 import { AuthController } from './controllers';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities';
+import { AccessToken, RefreshToken, User } from './entities';
 import { UserRepository } from './repositories';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([User, AccessToken, RefreshToken]),
+  ],
   controllers: [AuthController],
   providers: [UserService, AuthService, UserRepository],
   exports: [UserService, AuthService, UserRepository],
