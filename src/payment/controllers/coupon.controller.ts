1812c0cfbff6b10052df5a6e4e5674122bd2c815
smi-23 <latack789@naver.com>
2024-04-29 13:52:23 +0900

    refactor: createCoupon함수의 반환값을 수정

--- src/payment/controllers/coupon.controller.ts
+++ src/payment/controllers/coupon.controller.ts
@@ -6,10 +6,26 @@ import { CouponReqDto } from '../dto';
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
+  // @Post('create')
+  // async createCoupon(
+  //   @Body() couponReqDto: CouponReqDto,
+  // ): Promise<{ message: string; content: CouponReqDto }> {
+  //   const couponInfo = await this.couponService.createCoupon(couponReqDto);
+  //   return {
+  //     message: '쿠폰이 생성되었습니다.',
+  //     content: {
+  //       name: couponInfo.name,
+  //       type: couponInfo.type,
+  //       description: couponInfo.description,
+  //       value: couponInfo.value,
+  //     },
+  //   };
+  // }
+
   @Post('create')
   async createCoupon(
     @Body() couponReqDto: CouponReqDto,
-  ): Promise<CouponReqDto> {
+  ): Promise<{ message: string; content: CouponReqDto }> {
     return await this.couponService.createCoupon(couponReqDto);
   }
 
--- src/payment/services/coupon.service.ts
+++ src/payment/services/coupon.service.ts
@@ -1,14 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CouponRepository } from '../repositories';
 import { CouponReqDto } from '../dto';
-import { Coupon } from '../entities';
+// import { Coupon } from '../entities';
 
 @Injectable()
 export class CouponService {
+  private readonly logger = new Logger(CouponService.name);
   constructor(private readonly couponRepository: CouponRepository) {}
 
-  async createCoupon(couponReqDto: CouponReqDto): Promise<Coupon> {
-    return this.couponRepository.createCoupon(couponReqDto);
+  // async createCoupon(couponReqDto: CouponReqDto): Promise<Coupon> {
+  //   return this.couponRepository.createCoupon(couponReqDto);
+  // }
+
+  async createCoupon(
+    couponReqDto: CouponReqDto,
+  ): Promise<{ message: string; content: CouponReqDto }> {
+    const couponInfo = await this.couponRepository.createCoupon(couponReqDto);
+    this.logger.log(`${couponInfo.name}쿠폰이 발급되었습니다.`);
+    return {
+      message: `${couponInfo.name}쿠폰이 생성되었습니다.`,
+      content: {
+        name: couponInfo.name,
+        type: couponInfo.type,
+        description: couponInfo.description,
+        value: couponInfo.value,
+      },
+    };
   }
 
   // for test
