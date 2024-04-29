101d4ddc7d53e3abccce6a0e731bc4fcf8b0d6a3
smi-23 <latack789@naver.com>
2024-04-29 09:28:50 +0900

    feat: couponEntity

--- src/payment/entities/coupon.entitiy.ts
+++ src/payment/entities/coupon.entitiy.ts
@@ -1,9 +1,13 @@
-import { BaseEntity } from "src/common/entities";
-import { Entity } from "typeorm";
+import { BaseEntity } from '../../common/entities';
+import { Column, Entity } from 'typeorm';
 
 type CouponType = 'precent' | 'fixed';
 
 @Entity()
 export class Coupon extends BaseEntity {
-  @Column
+  @Column({ type: 'varchar', length: 50 })
+  type: CouponType;
+
+  @Column({ type: 'decimal', precision: 5, scale: 2 })
+  value: number; // 할인율 또는 정액금액
 }
