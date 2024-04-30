902a3eaeeecb7a1260b5d59d7aa72dd33af456c4
smi-23 <latack789@naver.com>
2024-04-30 21:18:54 +0900

    feat: 포인트와 쿠폰 할인 적용 => 쿠폰 선처리

--- src/payment/services/payment.service.ts
+++ src/payment/services/payment.service.ts
@@ -19,6 +19,26 @@ export class PaymentService {
     private readonly pointLogRepository: PointLogRepository,
   ) {}
 
+  // 최종적으로 할인이 얼마가 적용되는지
+  private async applyDiscounts(
+    totalAmount: number,
+    userId: string,
+    couponId: string,
+    pointAmountToUse?: number,
+  ): Promise<number> {
+    const couponDiscount = couponId
+      ? await this.applyCoupon(couponId, userId, totalAmount)
+      : 0;
+    const pointDiscount = pointAmountToUse
+      ? await this.applyPoints(pointAmountToUse, userId)
+      : 0;
+
+    // 사실상 적립금을 후처리, 적립금을 먼저 처리하고 쿠폰을 사용하면 어떻게 될까?( 사용자가 받는 할인 감소)
+    const finalAmount = totalAmount - (couponDiscount + pointDiscount);
+    return finalAmount < 0 ? 0 : finalAmount;
+  }
+
+  // 쿠폰이 얼마나 적용되는지 계산 => 할인된 금액을 반환하는 것
   private async applyCoupon(
     couponId: string,
     userId: string,
@@ -58,11 +78,11 @@ export class PaymentService {
     } else if (coupon.type === 'fixed') {
       return coupon.value;
     }
-    return 0;
+    return 0; // 쿠폰의 타입이 percent, fixed가 아닐 경우
   }
 
-  // 해당 유저가 이 포인트를 사용 가능한지 체크
-  private async verifyPointAvailability(
+  // 해당 유저가 이 포인트를 사용 가능한지 체크하고 그 값을 반환
+  private async applyPoints(
     pointAmountToUse: number,
     userId: string,
   ): Promise<number> {
