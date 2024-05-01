665826984f312be9687727cc31bd36eafa031ae4
smi-23 <latack789@naver.com>
2024-05-01 18:04:43 +0900

    feat: 주문 생성, 완료 메소드 생성 in orderRepository

--- src/payment/repositories/order.repository.ts
+++ src/payment/repositories/order.repository.ts
@@ -1,14 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { EntityManager, Repository } from 'typeorm';
-import { InjectRepository, } from '@nestjs/typeorm';
-import { Order } from '../entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order, OrderItem, ShippingInfo } from '../entities';
+import { UserRepository } from 'src/auth/repositories';
+import { IssuedCouponRepository } from './issued-coupon.repository';
+import { PointRepository } from './point.repository';
 
 @Injectable()
 export class OrderRepository extends Repository<Order> {
   constructor(
     @InjectRepository(Order)
     private readonly repo: Repository<Order>,
+    private readonly userRepository: UserRepository,
+    private readonly issuedCouponRepository: IssuedCouponRepository,
+    private readonly pointRepository: PointRepository,
   ) {
     super(repo.target, repo.manager, repo.queryRunner);
+  }
+
+  async createOrder(
+    userId: string,
+    orderItems: OrderItem[],
+    finalAmount: number,
+    shippingInfo: ShippingInfo,
+  ): Promise<Order> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    const order = new Order();
+    order.user = user;
+    order.amount = finalAmount;
+    order.status = 'started';
+    order.items = orderItems;
+    order.shippingInfo = shippingInfo;
+    return this.save(order);
+  }
+
+  async completeOrder(orderId: string): Promise<Order> {
+    const order = await this.findOne({ where: { id: orderId } });
+    order.status = 'paid';
+
+    await Promise.all([
+      this.issuedCouponRepository.use(order.usedIssuedCoupon),
+      this.pointRepository.use(
+        order.user.id,
+        order.pointAmountUsed,
+        '주문 사용',
+      ),
+    ]);
+    return this.save(order);
   }
 }
